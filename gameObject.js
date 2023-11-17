3909d9b8b5571484138027f7486620d4ac037289
DiaDoesDastard <151188727+DiaDoesDastard@users.noreply.github.com>
2023-11-17 16:13:35 +0000

    Getting render ready

---
+++ gameObject.js
@@ -0,0 +1,28 @@
+function gameObject(position = [0,0,0], vertices = [[2,2,2]], triangles[[0]]){
+  this.position = position
+  this.triangles = triangles
+  this.vertices = vertices
+  this.normals = []
+  
+  this.generateNormals = function(){
+    var ab = [0,0,0]
+    var ac = [0,0,0]
+    var tempNormal = [0,0,0]
+    for(var triangleCounter = 0; triangleCounter<this.triangles.length; triangleCounter++){
+      ab[0] = vertices[triangles[triangleCounter][1]][0]-vertices[triangles[triangleCounter][0]][0]
+      ab[1] = vertices[triangles[triangleCounter][1]][1]-vertices[triangles[triangleCounter][0]][1]
+      ab[2] = vertices[triangles[triangleCounter][1]][2]-vertices[triangles[triangleCounter][0]][2]
+      
+      ac[0] = vertices[triangles[triangleCounter][2]][0]-vertices[triangles[triangleCounter][0]][0]
+      ac[1] = vertices[triangles[triangleCounter][2]][1]-vertices[triangles[triangleCounter][0]][1]
+      ac[2] = vertices[triangles[triangleCounter][2]][2]-vertices[triangles[triangleCounter][0]][2]
+
+      tempNormal[0] = ab[2]*ac[3]-ab[3]*ac[2]
+      tempNormal[1] = ab[1]*ac[3]-ab[3]*ac[1]
+      tempNormal[2] = ab[1]*ac[2]-ab[2]*ac[1]
+      
+      this.normals.append(tempNormal)
+    }
+  }
+  generateNormals()
+}
--- render.js
+++ render.js
@@ -1,4 +1,30 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var xSize = canvas.width;
+var ySize = canvas.height;
+var zoom = 0
+
 var rotationMatrix = new Array(3,3)
+var objectList = []
+
+var mainGamma = 0
+var mainPhi = 0
+
+const screenArray = new Uint8ClampedArray(xSize*ySize*4)
+
+
+function prepareRender(){
+  
+}
+
+function callRender(){
+  rotationMatrix = generateRotationalMatrix(mainGamma,mainPhi)
+}
+
+
+
+
 
 function generateRotationalMatrix(gamma,phi){
   var rotationalMatrix = new Array(3,3);
@@ -14,3 +40,19 @@ function generateRotationalMatrix(gamma,phi){
   return rotationalMatrix
 }
 
+function rotateVertices(rotationalMatrix, position, offsets){
+  var finalPosition = [0,0,0]
+  position = [position[0]-offsets[0],
+             position[1]-offsets[1],
+             position[2]-offsets[2]]
+  finalPosition[0] = rotationalMatrix[0][0]*position[0]+
+    rotationalMatrix[0][1]*position[1]+
+    rotationalMatrix[0][2]*position[2]
+  finalPosition[1] = rotationalMatrix[1][0]*position[0]+
+  rotationalMatrix[1][1]*position[1]+
+  rotationalMatrix[1][2]*position[2]
+  finalPosition[2] = rotationalMatrix[2][0]*position[0]+
+  rotationalMatrix[2][1]*position[1]+
+  rotationalMatrix[2][2]*position[2]
+  return finalPosition
+}
