063f3fd0a5d93c6a36333e5c8e02526b9191ad75
Emirhan <125173937+DeVBulut@users.noreply.github.com>
2023-11-17 19:48:01 -0700

    display object optimized

--- displayObject.js
+++ displayObject.js
@@ -2,15 +2,11 @@ function displayObject(_gameObject){
 
     
 
-    if(_gameObject == "enemy")
+    if(_gameObject.objectType == "enemy")
     {
-            document.onkeypress = function(evt) {
-            evt = evt || window.event;
-            var charCode = evt.keyCode || evt.which;
-            var charStr = String.fromCharCode(charCode);
-            if(charStr == "x"){
-                return _gameObject.objectType + "\n" + "Entity Hit Points " + _gameObject.currentHealth + "/" + _gameObject.maxHealth; 
-            }
+           // if(charStr == "x"){
+            return "Entity Hit Points " + _gameObject.currentHealth + "/" + _gameObject.maxHealth; 
+            //}
     }
     // else if()
     // {
@@ -20,7 +16,4 @@ function displayObject(_gameObject){
     // {
 
     // }
-
-
-    }
 }
