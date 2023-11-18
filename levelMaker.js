7ee8a16f7bb5d2015e63fbfed0df50c9459994da
DiaDoesDastard <151188727+DiaDoesDastard@users.noreply.github.com>
2023-11-18 02:01:03 +0000

    Added in run script

--- levelMaker.js
+++ levelMaker.js
@@ -1,2 +1,3 @@
-
-var tilemap = new Array(5,5)
+//Tilemap holds the objectID of all of the gameobjects for ease of targeting
+//Also holds the primive 
+var tilemap = new Array(5,5,3)
---
+++ run.js
@@ -0,0 +1,63 @@
+start()
+setInterval(play,1000/60); 
+//play()
+
+function start(){
+  for(h=-5;h<5;h++){
+    for(g=-5;g<5;g++){
+      generatePrimative("plane",[2*h,0,2*g]);      
+    }
+  }
+
+
+}
+
+function play(){
+  rotationMatrix = generateRotationMatrix(mainRho,mainPhi);
+  toCameraVector = [Math.sin(Math.PI * mainRho / 180),
+                    Math.sin(Math.PI * mainPhi / 180),
+                    Math.cos(Math.PI * mainRho / 180)]
+
+  for(e = 0; e<objectList.length; e++){
+    bufferObject(objectList[e], e, rotationMatrix, occlusionMask, offset);
+  }  
+  renderScreen()
+  let imageData = new ImageData(imageArrays,xSize)
+  ctx.putImageData(imageData,0,0)
+  occlusionMask.fill(-1)
+  clearScreen()
+}
+
+document.onkeypress = function(evt) {
+    evt = evt || window.event;
+    var charCode = evt.keyCode || evt.which;
+    var charStr = String.fromCharCode(charCode);
+    if(charStr == "a"){
+      offset[0] = offset[0]-scaling*Math.sin(Math.PI * mainRho / 180 + Math.PI/2);
+      offset[2] = offset[2]-scaling*Math.cos(Math.PI * mainRho / 180 + Math.PI/2);
+    }
+    if(charStr == "d"){
+      offset[0] = offset[0]+scaling*Math.sin(Math.PI * mainRho / 180 + Math.PI/2);
+      offset[2] = offset[2]+scaling*Math.cos(Math.PI * mainRho / 180 + Math.PI/2);
+    }
+    if(charStr == "w"){
+      offset[0] = offset[0]+scaling*Math.sin(Math.PI * mainRho / 180);
+      offset[2] = offset[2]+scaling*Math.cos(Math.PI * mainRho / 180);
+    }
+    if(charStr == "s"){
+      offset[0] = offset[0]-scaling*Math.sin(Math.PI * mainRho / 180);
+      offset[2] = offset[2]-scaling*Math.cos(Math.PI * mainRho / 180);
+    }
+    if(charStr == "e"){
+      mainRho += 1;
+    }
+    if(charStr == "q"){
+      mainRho -= 1;
+    }
+    if(charStr == "r"){
+      mainPhi += 1;
+    }
+    if(charStr == "f"){
+      mainPhi -= 1;
+    }
+};
